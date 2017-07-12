9588de81427f1cd7bc361238b1226dc756d322f3
Courtney Chu <CourtneyHChu@gmail.com>
2017-07-12 18:09:28 +0000

    loop arrays

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var b = 0
+while (b < ingredients.length){
+  console.log(ingredients[b]);
+  b++;
+}
+
+
+
+for (var i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}
+
+
+for (var i = ingredients.length; i >= 0; i--) {
+  console.log(ingredients[i]);
+}
