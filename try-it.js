db7a9d0a7d35dc79c8940c6c638803067976858b
Courtney Chu <CourtneyHChu@gmail.com>
2017-07-14 06:01:50 +0000

    adding new files

--- try-it.js
+++ try-it.js
@@ -1,12 +1,9 @@
 function merge(arrayx, arrayy) {
   var arrayz = arrayy.concat(arrayx);
-for (var i = 0; i <= arrayz.length; i++){
-  if (i > i++){
-    return i;
-  }
-}
+  arrayz.sort(function(a,b){return a-b});
 return arrayz
 }
+
 console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
 console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
 console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
